ef754e750f9da47e6151975b2d9ebc76e6590328
cyu <qq411011@gmail.com>
2024-12-19 15:04:38 +0800

    第三次

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+import FirstComponent from './components/FirstComponent.vue';
 </script>
 
 <template>
-<h2>Hello vue!</h2>
+<h2>Hello vue!!!</h2>
+<FirstComponent></FirstComponent>
+<first-component></first-component>
 </template>
 
 <style scoped>
-
+h2{color:red}
 </style>
---
+++ vue-app/src/components/FirstComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>第一個 component</h2>
+</template>
+
+<style scoped>
+    h2{
+        background-color: aquamarine;
+    }
+</style>
+<script setup>
+
+</script>
