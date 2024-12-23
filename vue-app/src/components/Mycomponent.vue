9e4ff35d4a114c438518993bafafd3dfb25a70af
cyu <qq411011@gmail.com>
2024-12-23 10:13:29 +0800

    2024/12/23 第五次

--- vue-app/src/components/Mycomponent.vue
+++ vue-app/src/components/Mycomponent.vue
@@ -1,16 +1,29 @@
-<script>
+<!-- <script>
     export default{
         data() {
         return {message:"Options API"}
         }
     }
+</script> -->
+
+<script setup>
+import { ref } from 'vue';
+    const message = ref("setup 語法糖")
+    const count =ref(0)
+
+    const add =()=>{
+        count.value++
+    }
+    
 </script>
 
 <template>
     <div>
         <input type="text" v-model="message" />
+        <h2>第二個 Component {{ message }}</h2>
         <h2 style="color:green">建立的第一個組件</h2>
         <h2  style="color:blue">這是第二個組件</h2>
+        <button @click="add">{{ count }}</button>
     </div>
 </template>
 
---
+++ vue-app/src/components/SecondComponent.vue
@@ -0,0 +1,39 @@
+<!-- <script>
+import { ref } from 'vue';
+
+
+    export default{
+       //options api
+        // data(){
+        //    return {message:"Options API"} 
+        // }
+
+       //compostion api
+       setup(){
+        const message = ref("Composition API")
+        return {message}
+       }
+    }
+</script> -->
+
+<script setup>
+  import { ref } from 'vue';
+  const message = ref("setup 語法糖")
+  const count =ref(0)
+
+  const add = ()=>{
+    count.value++
+  }
+</script>
+
+<template>
+    <div>
+      <input type="text" v-model="message" />
+      <h2>第二個 Component {{ message }}</h2>
+      <button @click="add">{{ count }}</button>
+    </div>
+</template>
+
+<style lang="css" scoped>
+h2{color:blue}
+</style>
