132b83c6e86b7be4741da648e299c82a53c511bb
cyu <qq411011@gmail.com>
2024-12-23 11:25:45 +0800

    2024/12/23 第五次

---
+++ vue-app/src/components/BindingComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+    const message = ref("Text Interpolation")
+
+    const sayHi = ()=>{
+        return "Hi"
+    }
+</script>
+
+<template>
+    <div>
+<h2>Template Binding</h2>
+    <!--{{ Text Interpolation 插值 表達式 Expression  }}  -->
+     <p>{{ 2 }}</p>
+     <p>{{ message }}</p>
+     <p>{{ 1 + 2 }}</p>
+     <p>{{ sayHi() }}</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+</style>
